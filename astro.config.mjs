75d3c5ae284e7f38f5ea85e9e11ec252bc2cd488
李澤洋 <taro.melillo@gmail.com>
2025-09-11 02:20:19 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,6 @@ export default defineConfig({
         "node:url",
         "node:net",
         "node:dns",
-        "node:crypto",
         "node:fs",
         "node:os",
         "node:child_process",
